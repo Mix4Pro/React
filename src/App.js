f5561cb494c92f8bb7147677e6f0322d853660c2
Mix4Pro <n.nishanbaev06@gmail.com>
2022-08-09 13:24:58 +0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,101 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import PropTypes from 'prop-types';
+import axios from 'axios'
+import Movie from './Movie';
+import './App.css'
 
-function App() {
+// let foodThatILike = [
+//   {
+//     id: 1,
+//     name: 'Soup',
+//     image: 'https://www.eatthis.com/wp-content/uploads/sites/4/2022/01/hot-and-sour-soup.jpg?quality=82&strip=1&w=800',
+//     rating: 4.5
+//   },
+//   {
+//     id: 2,
+//     name: 'Sandwich',
+//     image: 'https://simply-delicious-food.com/wp-content/uploads/2020/07/Easy-salad-sandwiches-with-herb-mayo-1.jpg',
+//     rating: 5.0
+//   },
+//   {
+//     id:3,
+//     name: 'Mutton',
+//     image: 'https://asianfoodnetwork.com/content/dam/afn/global/en/recipes/kolhapuri-mutton-curry/AFN_kolhapuri_mutton_curry_main_image.jpg.transform/desktop-img/img.jpg',
+//     rating: 4.8
+//   }
+// ]
+
+// function Food({name,picture,rating}){
+//   return (
+//     <div>
+//         <h3>I like {name}</h3>
+//         <h4>{rating} / 5.0</h4>
+
+//         <img src={picture} alt='LoL' />
+//     </div>
+//   );
+// }
+
+// Food.propTypes = {
+//   name: PropTypes.string.isRequired,
+//   picture: PropTypes.string.isRequired,
+//   rating: PropTypes.number.isRequired
+// }
+
+// function renderFood(dish){
+//   return <Food key={dish.id} name={dish.name} picture={dish.image} rating={dish.rating}/>
+// }
+
+// function App() {
+//   return (
+//     <div className="App">
+//       {foodThatILike.map(renderFood)}
+//     </div>
+//   );
+// }
+
+class App extends React.Component{
+  state = {
+    isLoading: true,
+    movies: []
+  }
+  getMovies = async () => {
+    const {
+      data:{ 
+        data: {movies},
+      }
+    } = await axios.get('https://yts.mx/api/v2/list_movies.json?sort_ by=rating')
+    console.log(movies)
+    this.setState({movies:movies , isLoading: false});
+
+  }
+  componentDidMount() {
+    this.getMovies();
+  }
+  render(){
+  const { isLoading, movies} = this.state;
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <section className='container'>
+      { isLoading ? (
+       <div className='loader'>
+        <span className='loader_text'>Loading</span>
+       </div> 
+      ) : (
+        <div className="movies">
+        {movies.map((movie) => (
+          <Movie
+          key={movie.id} 
+          id={movie.id} 
+          year={movie.year} 
+          title={movie.title} 
+          summary={movie.summary} 
+          poster={movie.medium_cover_image}
+          genres={movie.genres} />
+        ))}
+      </div>
+      )}
+  </section>
+  )
 }
-
+}
 export default App;
---
+++ src/Movie.js
@@ -0,0 +1,30 @@
+import React from "react";
+import PropTypes from 'prop-types'
+import './Movie.css'
+
+function Movie({id,year,title,summary,poster , genres}) {
+    return <div className="Movie">
+        <img src={poster} alr={title} title={title} />
+        <div className="movie_column">
+            <h3 className="movie_title">{title}</h3>
+            <h5 className="movie_year">{year}</h5>
+            <ul className="movie_genres">
+                {genres.map((genre,index)=> {
+                return <li key={index} className="genres_genre">{genre}</li>
+                })}
+            </ul>
+            <p className="movie_summary">{summary}</p>
+        </div>
+    </div>
+}
+
+Movie.protoType = {
+    id : PropTypes.number.isRequired,
+    year: PropTypes.number.isRequired,
+    title: PropTypes.string.isRequired,
+    summary: PropTypes.string.isRequired,
+    poster: PropTypes.string.isRequired ,
+    genres: PropTypes.arrayOf(PropTypes.string).isRequired
+};
+
+export default Movie;
