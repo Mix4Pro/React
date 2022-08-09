3f4fa82a61004ee58347925187c9e47a56576bb8
Mix4Pro <n.nishanbaev06@gmail.com>
2022-08-09 13:51:06 +0500

    first commit

--- src/Movie.js
+++ src/Movie.js
@@ -4,7 +4,7 @@ import './Movie.css'
 
 function Movie({id,year,title,summary,poster , genres}) {
     return <div className="Movie">
-        <img src={poster} alr={title} title={title} />
+        <img src={poster} alt={title} title={title} />
         <div className="movie_column">
             <h3 className="movie_title">{title}</h3>
             <h5 className="movie_year">{year}</h5>
